d0baf5900f382ca37d4d9e1935d958b6d0aa173d
Matt Hanger <mhanger@umich.edu>
2019-04-16 10:39:13 -0400

    Started pulling data with requests and requests-cache

--- SI507project_tools.py
+++ SI507project_tools.py
@@ -1,2 +1,20 @@
-import requests
+# -*- coding: utf-8 -*-
+
+import requests, requests_cache
 import json
+
+
+#https://chroniclingamerica.loc.gov/search/titles/results/?terms=michigan&format=json&page=5
+
+requests_cache.install_cache('chroniclingamerica')
+
+baseurl = "https://chroniclingamerica.loc.gov/search/titles/results/"
+
+params={"format":"json","page":1}
+
+first_result = requests.get(baseurl,params).json()
+
+with open('test_string.txt',"w",encoding="utf8",newline="") as testfile:
+    testfile.write(json.dumps(first_result))
+
+print(first_result)
